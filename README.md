fb12e1b6ec8ad6b63c4875235c90565e7c8757d5
Ganson <1145317236@qq.com>
2024-09-10 01:50:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# 全目录
+
+[更多系统、论文，供君选择 ~~>](https://www.bitwise.net.cn)
+
+# 73.CollegeMaterialManagementSystem
+
+<p>群: 983063232(大佬群 2TB学习资料,讲解)(入群获取sql文件)</p>
+<p>QQ: 206157502(加好友获取sql文件)</p>
+
+<p><h1 align="center">73.学院物资管理系统</h1></p>
+
+
+<p align="center">
+	<img src="https://img.shields.io/badge/jdk-1.8-orange.svg"/>
+    <img src="https://img.shields.io/badge/spring-5.x-lightgrey.svg"/>
+    <img src="https://img.shields.io/badge/springmvc-3.x-blue.svg"/>
+    <img src="https://img.shields.io/badge/mybatis-3.x-yellow.svg"/>
+</p>
+
+# 简介
+
+
+> 本代码来源于网络,仅供学习参考使用,请入群(983063232)后联系群主索要sql文件!
+
+
+
+# 环境
+
+- <b>IntelliJ IDEA 2009.3</b>
+
+- <b>Mysql 5.7.26</b>
+
+- <b>Tomcat 7.0.73</b>
+
+- <b>JDK 1.8</b>
+
+
+
+
+## 缩略图
+
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/ff46747b-a645-409f-8e61-8bd13ee2078d.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/46214c99-fdf1-40f5-bb23-1cbe8094a83c.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/f6f069a0-339a-4430-81a6-5ce0aa165058.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/8ad75c42-13c2-467a-b94e-5e5be63bacfc.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/d0c3b0f3-b858-4ff2-8683-3b9a17c078c8.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/f83961a4-5be5-4696-914d-d61a9a3b12d0.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/55dfbbad-c48a-4961-8196-4547c583406f.png)
+![](https://bitwise.oss-cn-heyuan.aliyuncs.com/2024/9/10/06ff7298-2135-4bff-8508-998d889ba159.png)
+
+
+
---
+++ src/main/java/cn/huanzi/qch/baseadmin/BaseAdminApplication.java
@@ -0,0 +1,225 @@
+package cn.huanzi.qch.baseadmin;
+
+import cn.huanzi.qch.baseadmin.common.pojo.Result;
+import cn.huanzi.qch.baseadmin.sys.sysauthority.pojo.SysAuthority;
+import cn.huanzi.qch.baseadmin.sys.sysauthority.repository.SysAuthorityRepository;
+import cn.huanzi.qch.baseadmin.sys.sysmenu.pojo.SysMenu;
+import cn.huanzi.qch.baseadmin.sys.sysmenu.repository.SysMenuRepository;
+import cn.huanzi.qch.baseadmin.sys.sysmenu.vo.SysMenuVo;
+import cn.huanzi.qch.baseadmin.sys.syssetting.service.SysSettingService;
+import cn.huanzi.qch.baseadmin.sys.syssetting.vo.SysSettingVo;
+import cn.huanzi.qch.baseadmin.sys.sysshortcutmenu.service.SysShortcutMenuService;
+import cn.huanzi.qch.baseadmin.sys.sysshortcutmenu.vo.SysShortcutMenuVo;
+import cn.huanzi.qch.baseadmin.sys.sysuser.pojo.SysUser;
+import cn.huanzi.qch.baseadmin.sys.sysuser.repository.SysUserRepository;
+import cn.huanzi.qch.baseadmin.sys.sysuser.service.SysUserService;
+import cn.huanzi.qch.baseadmin.sys.sysuser.vo.SysUserVo;
+import cn.huanzi.qch.baseadmin.sys.sysuserauthority.service.SysUserAuthorityService;
+import cn.huanzi.qch.baseadmin.sys.sysusermenu.service.SysUserMenuService;
+import cn.huanzi.qch.baseadmin.util.*;
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@EnableAsync//开启异步调用
+@SpringBootApplication
+@MapperScan(value = "cn.huanzi.qch.baseadmin.mbg.mapper")
+public class BaseAdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BaseAdminApplication.class);
+    }
+
+    /**
+     * 解决不能注入session注册表问题
+     */
+    @Bean
+    SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+}
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+@Configuration
+class IndexController {
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    @Autowired
+    private SysSettingService sysSettingService;
+
+    @Autowired
+    private SysUserMenuService sysUserMenuService;
+
+    @Autowired
+    private SysShortcutMenuService sysShortcutMenuService;
+
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
+
+
+    /**
+     * 端口
+     */
+    @Value("${server.port}")
+    private String port;
+
+    /**
+     * 启动成功
+     */
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return applicationArguments -> {
+            try {
+                //系统启动时获取数据库数据，设置到公用静态集合sysSettingMap
+                SysSettingVo sysSettingVo = sysSettingService.get(1).getData();
+                SysSettingUtil.setSysSettingMap(sysSettingVo);
+
+                //获取本机内网IP
+                log.info("启动成功：" + "http://" + InetAddress.getLocalHost().getHostAddress() + ":" + port + contextPath);
+            } catch (UnknownHostException e) {
+                //输出到日志文件中
+                log.error(ErrorUtil.errorInfoToString(e));
+            }
+        };
+    }
+
+    /**
+     * 跳转登录页面
+     */
+    @GetMapping("loginPage")
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView("login");
+
+        //系统信息
+        modelAndView.addObject("sys", SysSettingUtil.getSysSetting());
+
+        //后端公钥
+        String publicKey = RsaUtil.getPublicKey();
+        log.info("后端公钥：" + publicKey);
+        modelAndView.addObject("publicKey", publicKey);
+
+        return modelAndView;
+    }
+
+
+    /**
+     * 跳转首页
+     */
+    @GetMapping("")
+    public void index1(HttpServletResponse response){
+        System.out.println(1);
+        //内部重定向
+        try {
+            response.sendRedirect("/index");
+        } catch (IOException e) {
+            //输出到日志文件中
+            log.error(ErrorUtil.errorInfoToString(e));
+        }
+    }
+    @GetMapping("index")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        //系统信息
+        modelAndView.addObject("sys", SysSettingUtil.getSysSetting());
+
+        //登录用户
+        SysUserVo sysUserVo = sysUserService.findByLoginName(SecurityUtil.getLoginUser().getUsername()).getData();
+        sysUserVo.setPassword(null);//隐藏部分属性
+        modelAndView.addObject( "loginUser", sysUserVo);
+
+        //登录用户系统菜单
+        List<SysMenuVo> menuVoList = sysUserMenuService.findByUserId(sysUserVo.getUserId()).getData();
+        modelAndView.addObject("menuList",menuVoList);
+
+        //登录用户快捷菜单
+        List<SysShortcutMenuVo> shortcutMenuVoList= sysShortcutMenuService.findByUserId(sysUserVo.getUserId()).getData();
+        modelAndView.addObject("shortcutMenuList",shortcutMenuVoList);
+
+        //后端公钥
+        String publicKey = RsaUtil.getPublicKey();
+        log.info("后端公钥：" + publicKey);
+        modelAndView.addObject("publicKey", publicKey);
+
+        return modelAndView;
+    }
+
+    /**
+     * 获取验证码图片和文本(验证码文本会保存在HttpSession中)
+     */
+    @RequestMapping("getVerifyCodeImage")
+    public void getVerifyCodeImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //设置页面不缓存
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.getOutputStream();
+        String verifyCode = VerifyCodeImageUtil.generateTextCode(VerifyCodeImageUtil.TYPE_NUM_UPPER, 4, null);
+
+        //将验证码放到HttpSession里面
+        request.getSession().setAttribute("verifyCode", verifyCode);
+         log.info("本次生成的验证码为：" + verifyCode + ",已存放到HttpSession中");
+
+        //设置输出的内容的类型为JPEG图像
+        response.setContentType("image/jpeg");
+        BufferedImage bufferedImage = VerifyCodeImageUtil.generateImageCode(verifyCode, 90, 30, 3, true, Color.WHITE, Color.BLACK, null);
+
+        //写给浏览器
+        ImageIO.write(bufferedImage, "JPEG", response.getOutputStream());
+    }
+
+    /**
+     * 跳转实时系统硬件监控
+     */
+    @GetMapping("monitor")
+    public ModelAndView monitor() {
+        return new ModelAndView("monitor.html","port",port);
+    }
+
+    /**
+     * 跳转实时日志
+     */
+    @GetMapping("logging")
+    public ModelAndView logging() {
+        return new ModelAndView("logging.html","port",port);
+    }
+}
